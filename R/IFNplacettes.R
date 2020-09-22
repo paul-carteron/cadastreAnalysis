e011bd839ae532cebf87d8d329f41a3a89bdffa0
CARTERON Paul <carteronpaul@gmail.com>
2020-09-22 18:25:09 +0200

    Data IFN implante proprement avec description Ajout resolution dans fonction MNT

---
+++ R/IFNplacettes.R
@@ -0,0 +1,12 @@
+#' Donnes concernant les placettesIFN
+#'
+#' @docType data
+#'
+#' @usage data(IFNplacettes)
+#'
+#' @format An object of class data.frame
+#'
+#' @examples
+#' data(IFNplacettes)
+#'
+"IFNplacettes"
---
+++ R/importMNT.R
@@ -0,0 +1,29 @@
+#' importMNT
+#'
+#' @param zoneEtude L'objet sf contenant nos parcelles cadastrales
+#' @param buffer Zone de prospection en metre
+#' @param zoom Nibveau de precision du MNT. Voir fonction "get_elev_raster" du package elevatr pour plus d'infos.
+#'
+#' @return Renvoi le MNT de la zoneEtude
+#' @export
+#'
+#' @import sf rgdal
+#' @importFrom elevatr get_elev_raster
+#'
+#' @examples
+importMNT <- function(zoneEtude, buffer = 100, zoom = 10) {
+
+   latitude = st_transform(zoneEtude,4326) %>%
+      st_coordinates() %>%
+      as.data.frame() %>%
+      pull(Y) %>%
+      mean()
+
+   ground_resolution = (cos(latitude * pi/180) * 2 * pi * 6378137) / (256 * 2^zoom)
+
+   buffer = st_buffer(st_transform(zoneEtude,2154),buffer)
+   MNT =  get_elev_raster(buffer, z = zoom, clip = "bbox")
+
+   print(paste("La resolution du MNT est de :",round(ground_resolution,2),"m"))
+   return(MNT)
+}
